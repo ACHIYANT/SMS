daecced4323aef324986015fc751e36f1ecd2c95
ACHIYANT <achiyant@gmail.com>
2025-04-14 22:31:24 +0530

    JWT create token and verify token logic done

--- backend/AuthService/src/index.js
+++ backend/AuthService/src/index.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser");
 
 // const bcrypt = require("bcrypt");
 // const { User } = require("./models/index");
+// const UserRepository  = require("./repository/user-repository");
+// const UserService = require("./services/user-service");
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
@@ -14,9 +16,22 @@ const prepareAndStartServer = () => {
   app.use("/api", apiRoutes);
   app.listen(PORT, async () => {
     console.log(`Server Started At PORT ${PORT}`);
+    // const repo = new UserRepository();
+    // const response = await repo.getById(3);
+    // console.log(response);
     // const incomingPassword = "Hartron@123";
     // const user = await User.findByPk(5);
     // const response = bcrypt.compareSync(incomingPassword, user.password);
+    // console.log(response);
+
+    // const service = new UserService();
+    // const newToken = service.createToken({email: 'abc@gmail.com', id:1});
+    // console.log("new token is ", newToken);
+
+    // const token =
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY0BnbWFpbC5jb20iLCJpZCI6MSwiaWF0IjoxNzQ0NjQ5ODc4LCJleHAiOjE3NDQ2NDk5MDh9.x6ioFOQX2b2k2tx7Ze5faYwpgNZWgssEKFSuwI2SlMc";
+
+    // const response = service.verifyToken(token);
     // console.log(response);
   });
 };
--- backend/AuthService/src/services/user-service.js
+++ backend/AuthService/src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository = require("../repository/user-repository");
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
   constructor() {
@@ -8,9 +10,28 @@ class UserService {
   async create(data) {
     try {
       const user = await this.UserRepository.create(data);
-	  return user;
+      return user;
     } catch (error) {
       console.log("Something went wrong in the service layer.");
+      throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation.");
+      throw error;
+    }
+  }
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation : ", error);
       throw error;
     }
   }
